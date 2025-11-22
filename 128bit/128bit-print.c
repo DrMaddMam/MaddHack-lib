ef7569790d77f4b76ccdb7fa1d7687be19ffeac1
DrMaddMam <Luth.passhacker@gmail.com>
2025-11-22 10:39:08 -0500

    i have removed ALL stdio

--- 128bit/128bit-print.c
+++ 128bit/128bit-print.c
@@ -1,23 +1,33 @@
 #ifndef BIT_PRINT_C
 #define BIT_PRINT_C
-#include <stdio.h>
 #include "../madd-types.h"
-inline U0 print_u128(U128 n) {
-    char buf[50];
-    U8 i = sizeof(buf)-1;
+
+/// @brief prints U128 to buffer
+/// @param n 
+/// @param buf 
+/// @param bufsize 
+/// @return the buffer pointer
+U8* print_u128(U128 n, U8* buf, UMin bufsize) {
+    U8 i = bufsize-1;
     buf[i] = '\0';
     do {
         buf[--i] = '0' + (n % 10);
         n /= 10;
     } while (n);
-    puts(&buf[i]);
+    return buf;
 }
-inline U0 print_i128(I128 n) {
+
+/// @brief prints I128 to buffer
+/// @param n 
+/// @param buf 
+/// @param bufsize 
+/// @return the buffer pointer
+U8 print_i128(I128 n, U8* buf, UMin bufsize) {
     if (n < 0) {
-        putchar('-');
-        print_u128((U128)(-n));
+        buf[0] = '-';
+        return print_u128((U128)(-n), buf+1, bufsize-1);
     } else {
-        print_u128((U128)n);
+        return print_u128((U128)n, buf, bufsize);
     }
 }
 #endif // 128BIT_PRINT_C
--- maddhack.h
+++ maddhack.h
@@ -1,20 +1,27 @@
 #ifndef MADDHACK_H
 #define MADDHACK_H
-#include <stdio.h>
 #include "madd-types.h"
 #include "fast-memlib/fast-memlib.h"
 #include "128bit/128bit.h"
 #include "hackery/madd-hackery.h"
 #include "flags/flags.h"
+
+/// @brief initialize MaddHack resources
+/// @return if initialization was successful
 inline Bool initMaddHack() {
-    return (initFlags() && resetMem()); // todo: add more init functions later
+    return (
+        initFlags() &&
+        resetMem()
+    );
 }
-inline Bool exitMaddHack() {
-    UMax unfreed = cleanupMem();
-    if(unfreed > 0) { 
-        fprintf(stderr, "Forgot to free "UMaxFORMAT" pieces of memory", unfreed);
-        return false;
-    }
+/// @brief cleanup MaddHack resources
+/// @return amount of errors during cleanup
+inline UMax exitMaddHack() {
+#ifdef USE_FAST_MEMLIB
+    UMax errors = 0;
+    errors += cleanupMem();
+    return errors;
+#endif
     return true;
 }
 #endif // MADDHACK_H
